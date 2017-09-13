b3f5d3ae3c9b0a087e3ccb0703d184fe4f2ba88e
James <1404334+j-selby@users.noreply.github.com>
2017-09-14 00:47:23 +1000

    Fix checking for night in mornings

--- src/main.rs
+++ src/main.rs
@@ -69,10 +69,19 @@ fn check_night(start_night : u32, end_night : u32) -> bool {
     let time = Local::now();
     let start_time = NaiveTime::from_hms(start_night, 0, 0);
     let end_time = NaiveTime::from_hms(end_night, 0, 0);
+    let cur_date = time.naive_local();
+    let cur_time = cur_date.time();
 
-    let start_date = time.date().naive_local().and_time(start_time);
+    let start_date = if (cur_time < start_time) && !(start_time < end_time) {
+        // Early morning
+        let start_date = time.date().naive_local();
+        let start_date = start_date - cDuration::days(1);
+        start_date.and_time(start_time)
+    } else {
+        time.date().naive_local().and_time(start_time)
+    };
 
-    let end_date = if start_time > end_time {
+    let end_date = if start_time > end_time && !(cur_time < end_time) {
         // End night is on the next day
         let end_date = time.date().naive_local();
         let end_date = end_date + cDuration::days(1);
@@ -80,8 +89,6 @@ fn check_night(start_night : u32, end_night : u32) -> bool {
     } else {
         time.date().naive_local().and_time(end_time)
     };
-
-    let cur_date = time.naive_local();
 
     cur_date > start_date && cur_date < end_date
 }
