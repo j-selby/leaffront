66214b24167670d94eb326eeac67b1b13e1f8905
James <jselby@jselby.net>
2017-10-08 17:51:18 +1100

    Add cargo configs for additional modules

---
+++ render_pi/src/lib.rs
@@ -0,0 +1,16 @@
+extern crate leaffront_core;
+
+extern crate egl;
+extern crate opengles;
+extern crate videocore;
+
+extern crate image;
+
+// TODO: Hide all these, bar drawer
+mod gl_context;
+
+pub mod shader;
+pub mod texture;
+pub mod vbo;
+
+pub mod drawer;
