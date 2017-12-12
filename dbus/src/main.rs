35fcb080f2dcfb19d6a69a21631f7e06a607c21b
James <1404334+j-selby@users.noreply.github.com>
2017-12-12 14:48:59 +1100

    Add D-Bus notification bridge

---
+++ dbus/src/main.rs
@@ -0,0 +1,36 @@
+extern crate notify_rust;
+extern crate redis;
+
+extern crate serde;
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
+
+use notify_rust::server::NotificationServer;
+
+#[derive(Serialize, Deserialize)]
+pub struct Notification {
+    name : String,
+    contents : String
+}
+
+fn main() {
+    let mut server = NotificationServer::new();
+
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let sub = client.get_connection().unwrap();
+
+    println!("Ready!");
+
+    server.start(| ref notify | {
+        let notification = Notification {
+            name : notify.appname.to_owned(),
+            contents : notify.body.to_owned()
+        };
+        let v : String = serde_json::to_string(&notification).unwrap();
+        let k : &str = "leaffront.notify";
+
+        redis::cmd("PUBLISH").arg(k).arg(&v).execute(&sub);
+    });
+}
