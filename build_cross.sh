1a0683c8be07b6ba589ee8ccc1ab73799fdec83d
James <1404334+j-selby@users.noreply.github.com>
2018-10-20 01:51:01 +1100

    Update build scripts

--- build_cross.sh
+++ build_cross.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 . $HOME/.cargo/env
 
+set -e
+
 CARGO_INCREMENTAL=1 PKG_CONFIG_ALLOW_CROSS=1 cargo build --release --features raspberry_pi
 strip target/release/leaffront-station --strip-unneeded
 cargo deb --no-build --no-strip
